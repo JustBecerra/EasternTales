92c617754eeee8df91e66e7fe018a51adb16d92d
JustBecerra <justj.becerra@gmail.com>
2026-03-09 18:01:46 -0300

    check if discord is here

---
+++ main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func new_game():
+	$Player.start($StartPosition.position)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
